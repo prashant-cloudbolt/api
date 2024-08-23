d64e06651a45dc4d880fd947befbbad459d198df
Prashant Rajavat <prajavat@cloudbolt.io>
2024-08-23 17:43:06 +0530

    testing qa-automation

--- get_commit_id.sh
+++ get_commit_id.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-latest_release=$(curl -s https://api.github.com/repos/prashant-cloudbolt/api/releases/latest)
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $branch_name"
+
+case $branch_name in
+multi_tenant_*)
+    branch_suffix=$(echo "$branch_name" | sed 's/multi_tenant_//; s/-//g')
+    ;;
+mira-stage)
+    branch_suffix="STAGE"
+    ;;
+*)
+    echo "Unsupported branch: $branch_name"
+    exit 1
+    ;;
+esac
+
+# Convert suffix to uppercase
+branch_suffix=$(tr '[:lower:]' '[:upper:]' <<< "$branch_suffix")
+echo "branch_suffix=$branch_suffix"
+
+# Get the latest release matching the branch suffix
+latest_release=$(gh release list --limit 30 | grep -E "$branch_suffix" | sort -k4 -r | head -n 1 | awk '{print $1}')
 echo "latest_release=$latest_release"
-commit_id=$(echo $latest_release | jq -r '.target_commitish')
-echo "The commit ID for the latest release is: $commit_id"
+
+release_tag=$(gh release view $latest_release --json tagName -q ".tagName")
+echo "release_tag=$release_tag"
+gitcommit_hash=$(git show $release_tag --pretty=format:"%H" --no-patch)
+echo "gitcommit_hash=$gitcommit_hash"
