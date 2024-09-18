ca7d2d0c9590ab052903ae8486ed1b988079b4b5
Prashant Rajavat <prajavat@cloudbolt.io>
2024-09-18 13:49:56 +0530

    s3 deploy for feature flag json testing

--- scripts/verify_feature_flag_change.sh
+++ scripts/verify_feature_flag_change.sh
@@ -7,6 +7,7 @@ set -e
 # Get the stack name, bucket name, and feature flag file name from positional arguments
 BUCKET_NAME=$1
 FILE_NAME=$2
+WORKING_DIR=$3
 
 if [[ -z "$BUCKET_NAME" ]]; then
   echo "Error: Bucket name is required."
@@ -15,6 +16,11 @@ fi
 
 if [[ -z "$FILE_NAME" ]]; then
   echo "Error: Feature flag file name is required."
+  exit 1
+fi
+
+if [[ -z "$WORKING_DIR" ]]; then
+  echo "Error: Feature flag working director is required."
   exit 1
 fi
 
@@ -32,16 +38,16 @@ get_s3_md5() {
 }
 
 # Calculate MD5 checksums for local files
-local_feature_flag_md5=$(calculate_md5 "services/feature_flag/$FILE_NAME")
+local_feature_flag_md5=$(calculate_md5 "$WORKING_DIR/services/feature_flag/$FILE_NAME")
 
 # Get MD5 checksums for files in S3
 s3_feature_flag_md5=$(get_s3_md5 "$BUCKET_NAME" "$FILE_NAME")
 
 # Upload files only if they have changed
 if [ "$local_feature_flag_md5" != "$s3_feature_flag_md5" ]; then
-  aws s3 cp "services/feature_flag/$FILE_NAME" "s3://$BUCKET_NAME/"
+  aws s3 cp "$WORKING_DIR/services/feature_flag/$FILE_NAME" "s3://$BUCKET_NAME/"
   new_feature_flag_version_id=$(aws s3api list-object-versions --bucket "$BUCKET_NAME" --prefix "$FILE_NAME" --query "Versions[?IsLatest].VersionId" --output text)
-  echo "Uploaded services/feature_flag/$FILE_NAME with version ID: $new_feature_flag_version_id"
+  echo "Uploaded $WORKING_DIR/services/feature_flag/$FILE_NAME with version ID: $new_feature_flag_version_id"
 else
   feature_flag_version_id=$(aws s3api list-object-versions --bucket "$BUCKET_NAME" --prefix "$FILE_NAME" --query "Versions[?IsLatest].VersionId" --output text)
   echo "No changes detected in $FILE_NAME. Skipping upload. Current version ID: $feature_flag_version_id"
